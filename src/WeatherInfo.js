9729f4d0d77c985d03e80b033a76b5d20fca66fa
sabreenshehwar <76182477+sabreenshehwar@users.noreply.github.com>
2021-04-15 17:29:33 -0400

    Weather Temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -15,8 +16,7 @@ export default function WeatherInfo(props) {
             </div>
             <div className="row">
                 <div className="col-6">
-                    <span className="temperature">{Math.round(props.data.temperature)}</span>
-                    <span className="units">°C</span>
+                    <WeatherTemperature celsius={props.data.temperature} />
                 </div>
                 <div className="col-6">
                     <ul>
