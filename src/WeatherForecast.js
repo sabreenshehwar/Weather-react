a6c247fae6b4625510e9ab2550973e68caa5dc72
sabreenshehwar <76182477+sabreenshehwar@users.noreply.github.com>
2021-04-16 13:13:33 -0400

    Forecast mulitples days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css"
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
-    return (
-        <div className="WeatherForecast">
-            <div className="card">
-                <div className="card-body">
-                    <div className="card-title">
-                        <div className="WeatherForecast-day">
-                            Saturday
-                        </div>
-                    </div>
-                    <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperature-max">10°C </span>
-                        |
-                    <span className="WeatherForecast-temperature-min">2° C</span>
-                    </div>
+export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
+    function handleResponse(response) {
+        setForecast(response.data.daily);
+        setLoaded(true);
+    }
+
+    if (loaded) {
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                    {forecast.map(function (dailyForecast, index) {
+                        if (index < 4) {
+                            return (
+                                <div className="col" key={index}>
+                                    <WeatherForecastDay data={dailyForecast} />
+                                </div>
+                            );
+                        }
+                    })}
                 </div>
             </div>
-        </div>
-    )
-        ;
+        );
+
+    } else {
+
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiKey = "ae84fe76c05b33cdc51b77049718e2b3";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+        return null;
+
+    }
 } 
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+        let days = [
+            "Sun",
+            "Mon",
+            "Tues",
+            "Wed",
+            "Thurs",
+            "Fri",
+            "Sat",
+        ];
+
+        return days[day];
+    }
+    return (
+        <div className="card">
+            <div className="card-body">
+                <div className="card-title">
+                    <div className="WeatherForecast-day">
+                        {day()}
+                    </div>
+                </div>
+                <div className="WeatherForecast-temperatures">
+                    <span className="WeatherForecast-temperature-max">{Math.round(props.data.temp.max)} °C </span>
+                        |
+                    <span className="WeatherForecast-temperature-min">{Math.round(props.data.temp.min)} °C</span>
+                </div>
+            </div>
+        </div>
+    );
+}
