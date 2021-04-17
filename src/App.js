11b2b0964cfc37615fa3ef05a47c503d8b313f9f
sabreenshehwar <76182477+sabreenshehwar@users.noreply.github.com>
2021-04-17 08:38:28 -0400

    final touches

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 import "./App.css";
@@ -10,9 +9,9 @@ export default function App() {
       <div className="Container">
         <h1>World Weather</h1>
         <Weather defaultCity="Tokyo" />
-        <footer> <a
-          href="https://github.com/sabreenshehwar/weather-react"
+        <footer> <a href="https://github.com/sabreenshehwar/weather-react"
           target="_blank"
+          rel="noreferrer"
         >
           open-source code
             </a>
@@ -22,5 +21,3 @@ export default function App() {
     </div>
   );
 }
-
-
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,6 +24,7 @@ export default function WeatherForecast(props) {
                                 </div>
                             );
                         }
+                        return null;
                     })}
                 </div>
             </div>
